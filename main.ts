967377f98a7eef8d104d17130b42f7679b087680
Rob Wilson <rob@rwilson.org.uk>
2022-10-19 10:15:18 +0100

    improved terminal display

--- main.ts
+++ main.ts
@@ -1,6 +1,12 @@
 // Get data from logger microbit, relay to terminal
 radio.onReceivedString(function (receivedString) {
-    serial.writeLine(receivedString)
+    lastChar = receivedString.charAt(receivedString.length - 1)
+    // Keep dateTime string on the same terminal line as readings
+    if (lastChar.compare(",") == 0) {
+        serial.writeString(receivedString)
+    } else {
+        serial.writeLine(receivedString)
+    }
 })
 // Get command from terminal, relay to logger microbit
 serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
@@ -8,6 +14,7 @@ serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
     radio.sendString(serialString)
 })
 let serialString = ""
+let lastChar = ""
 radio.setGroup(1)
 radio.setTransmitPower(7)
 let outString = ""
